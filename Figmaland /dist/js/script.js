df19a7cd44914c6ce505e72e7c626bb283de7ef0
benniks <benxmr@gmail.com>
2020-11-19 04:31:55 +0300

    rm

--- Figmaland /dist/js/script.js
+++
@@ -1,6 +0,0 @@
-const mobileNav = document.querySelector(".header__inner-mobile-nav");
-const menuButton = document.querySelector(".header__inner-menu");
-
-menuButton.onclick = () =>{
-    mobileNav.classList.toggle('active')
-}
